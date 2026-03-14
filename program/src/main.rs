0e445ad995e3022475bb4347a6804d6238ad841a
eddy <eddyboughioul@hotmail.fr>
2026-03-14 22:50:07 +0100

    add profiling

--- program/src/main.rs
+++ program/src/main.rs
@@ -20,8 +20,10 @@ pub fn main() {
     let a: u64 = sp1_zkvm::io::read::<u64>();
     let b: u64 = sp1_zkvm::io::read::<u64>();
 
+    println!("cycle-tracker-start: poseidon");
     let vec: Vec<U256> = vec![U256::from(a), U256::from(b)];
     let result = fibonacci_lib::hash(vec);
+    println!("cycle-tracker-end: poseidon");
 
     let out: [u8; 32] = result.to_be_bytes();
     sp1_zkvm::io::commit_slice(&out);
